b28936c87303cfaa324b744b662d8eee75972345
FS12321 <user.911@t-online.de>
2021-06-27 19:32:02 +0200

    init

---
+++ SuS_Aufgaben/Debug/main.d
@@ -0,0 +1,66 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdint.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdbool.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/inc/hw_memmap.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/inc/hw_types.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/sysctl.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/adc.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/gpio.h
+main.obj: C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/timer.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/math.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_defs.h
+main.obj: C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_limits.h
+
+../main.c:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_ti_config.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/linkage.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_stdint40.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/cdefs.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_types.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/sys/_stdint.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/stdbool.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/inc/hw_memmap.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/inc/hw_types.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/sysctl.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/adc.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/gpio.h:
+
+C:/GitHubRepository/noRepos_FolderforFramework_unpacked/TivaWare\ Framework/driverlib/timer.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/math.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/_defs.h:
+
+C:/ti/ccs1031/ccs/tools/compiler/ti-cgt-arm_20.2.4.LTS/include/machine/_limits.h:
+
